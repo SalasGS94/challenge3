e683de5c91b9f63d9e3cea69ca4fe4c1a04fda67
unknown <gergasa94@gmail.com>
2023-03-21 17:56:58 -0600

    just lowercase

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,14 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var characterAmountNumber = document.getElementById('characterAmountNumber')
+const form = document.getElementById('passwordGeneratorForm')
+
+var LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+
+
+form.addEventListener("click", e => {
+  e.preventDefault()
+  var characterAmount = characterAmountNumber.value
+  var password = writePassword(characterAmount)
+  passwordDisplay.innerText = password
+})
+
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword(characterAmount) {
 
-  passwordText.value = password;
-
+  let charCodes = LOWERCASE_CHAR_CODES
+  
+  var passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    var characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
 }
+
+  function arrayFromLowToHigh(low, high) {
+    const array = []
+    for (let i = low; i <= high; i++) {
+      array.push(i)
+    }
+    return array
+  }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
